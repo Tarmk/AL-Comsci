3d031632277943495ccf97157dab48414ec00a4d
Krittaphas Kunkhongkaphan <tarm.kun@gmail.com>
2025-08-27 08:21:44 +0700

    Refine README.md by adding troubleshooting tips and clarifying usage instructions to improve user experience

---
+++ fish_oop.py
@@ -0,0 +1,33 @@
+class Animal:
+    def __init__(self, s: str, n: int) -> None:
+        self.__state: str = s
+        self.__size: int = n
+
+    def feed(self) -> None:
+        self.__size += 1
+        if self.__size == 5:
+            self.__state = "FISH"
+
+    def getState(self) -> str:
+        return self.__state
+
+    def getSize(self) -> int:
+        return self.__size
+
+
+def main() -> None:
+    thisFish = Animal("Fish", 1)
+
+    # First two OUTPUT lines in the pseudocode
+    print(f"{thisFish.getState()} is of size {thisFish.getSize()}")
+
+    while thisFish.getState() != "FISH":
+        thisFish.feed()
+
+    print(f"It is now a big {thisFish.getState()}")
+
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ fish_procedural.py
@@ -0,0 +1,20 @@
+def feed(state, size):
+    size[0] += 1
+    print("Fish fed")
+    if size[0] == 5:
+        state[0] = "FISH"
+
+
+def main():
+    state = ["Fish"]
+    size = [1]
+    print(f"{state[0]} is of size {size[0]}")
+
+    while state[0] != "FISH":
+        feed(state, size)
+
+    print(f"It is now a big {state[0]}")
+
+
+if __name__ == "__main__":
+    main()
