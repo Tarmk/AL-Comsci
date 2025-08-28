311cc52566925c8d91fdba868a71c04722147366
Krittaphas Kunkhongkaphan <tarm.kun@gmail.com>
2025-08-28 13:13:32 +0700

    Refactor Queue class initialization to require max_size parameter and update example usage in main block

--- OOP CIrcularQueue.py
+++ OOP CIrcularQueue.py
@@ -4,26 +4,35 @@ class Queue:
         self.data = [None] * max_size
         self.front = 0
         self.rear = -1
+        self.itemno = 0
 
     def enqueue(self, item):
-        if self.rear == self.max - 1:
+        if self.itemno == self.max:
             raise IndexError("Queue Full")
-        self.rear += 1
+        self.rear = (self.rear +1 ) % self.max
         self.data[self.rear] = item
+        self.itemno += 1
         
     def dequeue(self):
         if self.front == self.rear + 1:
             raise IndexError("Queue Empty")
         item = self.data[self.front]
-        self.front += 1
+        self.front = (self.front + 1) % self.max
+        self.itemno -= 1
         return item
     
 
 q = Queue(5)
 q.enqueue("Tarm")
 q.enqueue("Vivaan")
-print(q.dequeue())
 q.enqueue("Hello")
 q.enqueue("Hi")
+q.enqueue("TJ")
 print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+q.enqueue("test")
+q.enqueue("2")
 print(q.dequeue())
